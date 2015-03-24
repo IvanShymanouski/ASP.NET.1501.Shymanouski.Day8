bc9ab13835b703fe62a2bb72a2974f698a90a448
shymanouskiivan@gmail.com <shymanouskiivan@gmail.com>
2015-03-24 20:54:57 +0200

    task 0 complete

---
+++ ExpandedIterator/Extension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpandedIterator
+{
+    public static class Extension
+    {
+
+        /// <summary>
+        /// Extansion for generic collections
+        /// </summary>
+        /// <param name="getterRule">rule for getting elements</param>
+        public static IEnumerable<T> GetExpandedIterator<T>(this IEnumerable<T> someColection, Func<int,T,bool> getterRule)
+        {
+            int i = -1;
+            foreach (T element in someColection)
+            {
+                i++;
+                if (getterRule(i, element)) yield return element;
+            }
+        }
+
+        /// <summary>
+        /// Extansion for collections
+        /// </summary>
+        /// <param name="getterRule">rule for getting elements</param>
+        public static IEnumerable GetExpandedIterator(this IEnumerable someColection, Func<int, object, bool> getterRule)
+        {
+            int i = -1;
+            foreach (var element in someColection)
+            {
+                i++;
+                if (getterRule(i, element)) yield return element;
+            }
+        }
+
+    }
+}
---
+++ ExpandedIteratorConsole/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpandedIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("--List--");
+            List<int> collectionOfInt = new List<int>();
+
+            collectionOfInt.Add(1);
+            collectionOfInt.Add(2);
+            collectionOfInt.Add(3);
+            collectionOfInt.Add(4);
+            collectionOfInt.Add(5);
+
+            foreach (var element in collectionOfInt.GetExpandedIterator((x, y) => x % 2 == 0))
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("--Queue--");
+            Queue<double> collectQ = new Queue<double>();
+
+            collectQ.Enqueue(1.8);
+            collectQ.Enqueue(2.8);
+            collectQ.Enqueue(3.6);
+            collectQ.Enqueue(4.8);
+            collectQ.Enqueue(5.5);
+            collectQ.Enqueue(6.8);
+
+            foreach (var element in collectQ.GetExpandedIterator((x, y) => (x % 2 == 0) && (Math.Abs(y - (int)(y)) < 0.60001)))
+            {
+                Console.WriteLine(element);
+            }
+            
+
+            Console.WriteLine("--ArrayList--");
+            ArrayList arrayObj = new ArrayList();
+
+            arrayObj.Add("string1");
+            arrayObj.Add(234);
+            arrayObj.Add(new object());
+            arrayObj.Add("string2");
+            arrayObj.Add(23.4);
+            arrayObj.Add(new object());
+            arrayObj.Add("string3");
+
+            foreach (var element in arrayObj.GetExpandedIterator((x,y) => (x % 2 == 0) && (y is string)))
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
